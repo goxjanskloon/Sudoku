10ba5d4a653c25c312bbe1da3ac914d48553e1b2
goxjanskloon <goxjanskloon@outlook.com>
2023-11-28 20:16:42 +0800

    modified:Added solving option   main.cpp

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ std::string int2str(int a){
 }
 void genimg(){
     std::string path;
-    std::cout<<"欢迎使用数度生成器,请输入储存数独的文件夹路径(以‘/’结尾):\n";
+    std::cout<<"请输入将用于储存数独的文件夹路径(以‘/’结尾):\n";
     std::getline(std::cin,path);
     std::cout<<"请输入数独中空格的个数(0~64):";
     int k,n;
@@ -29,10 +29,27 @@ void genimg(){
         sud.output(10,10,600,img);
         saveimage(img,(path+int2str(i)+".png").c_str());
     }
-    std::cout<<"生成完毕,按任意键退出...";
+    std::cout<<"生成完毕,按任意键继续...\n";
     getch_console();
 }
+void solvesud(){
+    sudoku sud;
+    std::cout<<"请输入数独(空格分隔列,换行分割行,无需区分宫,空格子用0表示):\n";
+    sud.input();
+    std::cout<<"正在解数独,请稍等...(若有多个解,仅随机出一个)\n";
+    if(sud.solve()) std::cout<<"数独无解!";
+    else std::cout<<"数独的解为:\n",sud.print();
+    std::cout<<"按任意键继续...\n";
+}
 int main(){
-    genimg();
-    return 0;
+    while(1){
+        std::cout<<"\n欢迎使用九宫标准数独工具,按g生成数独,s解数独,q退出:\n";
+        switch(getch_console()){
+        case 'g':genimg();break;
+        case 's':solvesud();break;
+        case 'q':std::cout<<"\n欢迎下次使用!按任意键退出...";getch_console();goto end;
+        default:std::cout<<"无效操作!\n";
+        }
+    }
+end:return 0;
 }
